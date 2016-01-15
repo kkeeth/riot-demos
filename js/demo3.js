e9a462abd43d4d7e6aa2163a441d2a27d52fb1a3
k-kuwahara <kuwahara@lepra.jp>
2016-01-15 16:30:01 +0900

    update demo3 add examples

--- js/demo3.js
+++ js/demo3.js
@@ -1,4 +1,4 @@
-riot.tag2('app', '<h3>{opts.title}</h3> <form onsubmit="{doCheck}"> <input type="text" onkeyup="{input}"> <input __disabled="{!text}" type="submit" value="チェック"> </form>', '', '', function(opts) {
+riot.tag2('app', '<h3>{opts.title}</h3> <p>Please enter either of the text box</p> <form onsubmit="{doCheck}"> <p>example1</p> <input type="text" onkeyup="{input}"> <input __disabled="{!text}" type="submit" value="チェック"> <p>example2</p> <input type="text" onkeyup="{input}"> <span if="{text}"> <input type="submit" value="チェック"> </span> </form>', '', '', function(opts) {
         this.text = "";
 
         this.input = function(e) {
--- tag/demo3.tag
+++ tag/demo3.tag
@@ -1,8 +1,16 @@
 <app>
     <h3>{ opts.title }</h3>
+    <p>Please enter either of the text box</p>
     <form onSubmit={ doCheck } >
+        <p>example1</p>
         <input type="text" onkeyup={ input }>
         <input disabled={ !text } type="submit" value="チェック">
+
+        <p>example2</p>
+        <input type="text" onkeyup={ input }>
+        <span if={ text }>
+            <input type="submit" value="チェック">
+        </span>
     </form>
     <script>
         this.text = "";
