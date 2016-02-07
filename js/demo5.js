e92ed29e8ed605458581032fee90c61171cea5c1
kuwahara <kuwahara@lepra.jp>
2016-02-07 13:31:07 +0900

    update demo5

--- js/demo5.js
+++ js/demo5.js
@@ -1,8 +1,14 @@
-riot.tag2('app', '<h3>{opts.title}</h3> <p>filter of number</p> <ul> <li each="{opts.items.filter(check_num)}"><label">{value}</label></li> </ul>', '', '', function(opts) {
+riot.tag2('app', '<h3>{opts.title}</h3> <p>filter of number</p> <ul> <li each="{opts.items.filter(check_num)}"><label>{value}</label></li> </ul> <p>filter of string</p> <ul> <li each="{opts.items.filter(check_string)}"><label>{value}</label></li> </ul>', '', '', function(opts) {
 
 
    this.check_num = function(item) {
        if (typeof(item.value) == 'number') {
+           return item;
+       }
+   }.bind(this)
+
+   this.check_string = function(item) {
+       if (typeof(item.value) == 'string') {
            return item;
        }
    }.bind(this)
--- tag/demo5.tag
+++ tag/demo5.tag
@@ -2,13 +2,25 @@
    <h3>{ opts.title }</h3>
    <p>filter of number</p>
    <ul>
-      <li each="{ opts.items.filter(check_num) }"><label">{ value }</label></li>
+      <li each="{ opts.items.filter(check_num) }"><label>{ value }</label></li>
    </ul>
+   <p>filter of string</p>
+   <ul>
+      <li each="{ opts.items.filter(check_string) }"><label>{ value }</label></li>
+   </ul>
+ 
 
-   // check values
+   // check number 
    check_num(item) {
        if (typeof(item.value) == 'number') {
            return item;
+       }
+   }
+
+   // check string
+   check_string(item) {
+       if (typeof(item.value) == 'string') {
+           return item; 
        }
    }
 </app>
