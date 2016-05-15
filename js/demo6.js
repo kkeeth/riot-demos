9f682624e7a286a4ad6309ffb68ddf509ac4e17d
kuwahara <kuwahara@lepra.jp>
2016-05-16 00:17:23 +0900

    add demo6.js file

---
+++ js/demo6.js
@@ -0,0 +1,2 @@
+riot.tag2('app', '<h1>{opts.title}</h1>', 'h1 { font-family: Comic\\ Sans\\ MS; } p,li { font-size: 20px; line-height: 1.5; font-family: Comic\\ Sans\\ MS; }', '', function(opts) {
+}, '{ }');
